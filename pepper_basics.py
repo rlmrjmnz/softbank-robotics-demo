1621a33a5739437e236fe2f32772a2e77d108b13
Ari Mora <ariel.moraj@gmail.com>
2020-03-24 13:58:02 -0600

    Add initial demos

---
+++ pepper_basics.py
@@ -0,0 +1,31 @@
+# python3
+# coding: utf-8
+
+import cv2
+import time
+from qibullet import SimulationManager
+from qibullet import PepperVirtual
+
+
+def main():
+    simulation_manager = SimulationManager()
+    client = simulation_manager.launchSimulation(gui=True)
+    pepper = simulation_manager.spawnPepper(client, spawn_ground_plane=True)
+
+    pepper.goToPosture("Crouch", 0.6)
+    time.sleep(1)
+    pepper.goToPosture("Stand", 0.6)
+    time.sleep(1)
+    pepper.goToPosture("StandZero", 0.6)
+    time.sleep(1)
+    pepper.subscribeCamera(PepperVirtual.ID_CAMERA_BOTTOM)
+
+    while True:
+        img = pepper.getCameraFrame()
+        cv2.imshow("bottom camera", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
+
